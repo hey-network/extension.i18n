067937d9a4e29bc6533c4222497441fa29d56737
nab <nab@get-hey.com>
2019-03-18 14:10:41 +0100

    stake.hey.network i18n

---
+++ stake.hey.network/i18n.js
@@ -0,0 +1,121 @@
+// ██╗      █████╗ ███╗   ██╗ ██████╗ ██╗   ██╗ █████╗  ██████╗ ███████╗
+// ██║     ██╔══██╗████╗  ██║██╔════╝ ██║   ██║██╔══██╗██╔════╝ ██╔════╝
+// ██║     ███████║██╔██╗ ██║██║ ████╗██║   ██║███████║██║ ████╗█████╗
+// ██║     ██╔══██║██║╚██╗██║██║   ██║██║   ██║██╔══██║██║   ██║██╔══╝
+// ███████╗██║  ██║██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║╚██████╔╝███████╗
+// ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+/* LANGUAGE
+|  
+|  language code format: ISO 639-1
+|  refer to this list: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+|
+*/
+var txt = {
+	// GENERAL
+	"general": {
+		"confirm": {
+			"fr": "OK",
+			"en": "OK",
+			"jp": "OK"
+		},
+		"cancel": {
+			"fr": "Annuler",
+			"en": "Cancel",
+			"jp": "キャンセル"
+		}
+	},
+	"home": {
+		"start": {
+			"fr": "S'inscrire",
+			"en": "Start",
+			"jp": "開始"
+		},
+		"whatishey": {
+			"fr": "Qu'est ce que Hey?",
+			"en": "What is Hey?",
+			"jp": "ちょっと何ですか？"
+		},
+		"premiumForLife": {
+			"fr": "Premium à vie",
+			"en": "Premium for life",
+			"jp": "人生のためのプレミアム"
+		},
+		"forTheFirst": {
+			"fr": "Pour le premier Million d'inscrits",
+			"en": "for first 1M users",
+			"jp": "最初の100万人のユーザー向け"
+		},
+    "title": {
+			"fr": "Délégez vos <span class=\"grad\">LOOM</span> et <span class=\"grad\">gagnez en encore plus</span>",
+			"en": "Delegate <span class=\"grad\">LOOM</span> and <span class=\"grad\">earn rewards</span>",
+			"jp": "<span class=\"grad\">#ハッシュ</span> と <span class=\"grad\">トレンドトピックでチャット</span>"
+		},
+    "nfo": {
+			"fr": "Un jeu d'enfant, en 3 minutes, rentabilisez vos LOOM tant qu'il ne sont pas encore utiles.",
+			"en": "Nothing is easier, it only takes 3 minutes, make profit out of your LOOMs while you can't do anything with them so far.",
+			"jp": "簡単なことは何もありません、それは3分しかかかりません、あなたがこれまでのところ彼らと何もすることができない間あなたの織機から利益を得ます"
+		},
+		"stake": {
+			"fr": "Stakez vos LOOM",
+			"en": "Stake with us!",
+			"jp": "私たちとステーク"
+		},
+		"telegram": {
+			"fr": "Rejoindre sur Telegram",
+			"en": "Join us on Telegram",
+			"jp": "Telegram で私たちに参加"
+		},
+    "join": {
+			"fr": "Rejoignez nous sur Telegram pour poser des questions ou nous rencontrer: <span class=\"fluo\">#StakeHeyNetwork</span>",
+			"en": "Hey Loomers ! We, at <span class=\"fluo\">#Hey</span>, are almost ready to give you tons of information about <span class=\"fluo\">#Loom</span>, <span class=\"fluo\">#Staking</span> and <span class=\"fluo\">#Rewards</span>. Put your Looms at use and make some returns!",
+			"jp": "<span class=\"fluo\">#HeyNetwork</span> のディスカッションに参加しましょう"
+		},
+		"footer":{
+			"copyright":{
+				"fr": "Copyright",
+				"en": "Copyright",
+				"jp": "著作権"
+			},
+			"rights":{
+				"fr": "Tous droits réservés.",
+				"en": "All rights Reserved.",
+				"jp": "全著作権所有"
+			},
+			"aboutUs":{
+				"fr": "A propos",
+				"en": "About us",
+				"jp": "私たちに関しては"
+			},
+			"manifesto":{
+				"fr": "Manifesto",
+				"en": "Manifesto",
+				"jp": "マニフェスト"
+			},
+			"support":{
+				"fr": "Support",
+				"en": "Support",
+				"jp": "補助"
+			},
+			"contactUs":{
+				"fr": "Contactez-nous",
+				"en": "Contact us",
+				"jp": "お問い合わせ"
+			},
+			"stakingGuide":{
+				"fr": "Guide de <em>Staking</em>",
+				"en": "Staking Guide",
+				"jp": "<em>Staking</em>ガイド"
+			},
+			"website":{
+				"fr": "Site web Hey",
+				"en": "Hey website",
+				"jp": "<em>Hey</em> ウェブサイト"
+			},
+			"dashboard":{
+				"fr": "Tableau de bord",
+				"en": "Dashboard",
+				"jp": "計器盤"
+			}
+		}
+	}
+};
