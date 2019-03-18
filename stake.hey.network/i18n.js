ed5efb607515ea201d4fe578b25b32aa434ef0cb
S3x0r <olisek@gmail.com>
2019-03-18 15:24:56 +0100

    Update i18n.js
    Polish lng

--- stake.hey.network/i18n.js
+++ stake.hey.network/i18n.js
@@ -16,105 +16,125 @@ var txt = {
 		"confirm": {
 			"fr": "OK",
 			"en": "OK",
-			"jp": "OK"
+			"jp": "OK",
+			"pl": "OK"
 		},
 		"cancel": {
 			"fr": "Annuler",
 			"en": "Cancel",
-			"jp": "キャンセル"
+			"jp": "キャンセル",
+			"pl": "Anuluj"
 		}
 	},
 	"home": {
 		"start": {
 			"fr": "S'inscrire",
 			"en": "Start",
-			"jp": "開始"
+			"jp": "開始",
+			"pl": "Start"
 		},
 		"whatishey": {
 			"fr": "Qu'est ce que Hey?",
 			"en": "What is Hey?",
-			"jp": "ちょっと何ですか？"
+			"jp": "ちょっと何ですか？",
+			"pl": "Czym jest Hey?"
 		},
 		"premiumForLife": {
 			"fr": "Premium à vie",
 			"en": "Premium for life",
-			"jp": "人生のためのプレミアム"
+			"jp": "人生のためのプレミアム",
+			"pl": "Premium - na całe życie"
 		},
 		"forTheFirst": {
 			"fr": "Pour le premier Million d'inscrits",
 			"en": "for first 1M users",
-			"jp": "最初の100万人のユーザー向け"
+			"jp": "最初の100万人のユーザー向け",
+			"pl": "dla pierwszego miliona użytkowników"
 		},
     "title": {
 			"fr": "Délégez vos <span class=\"grad\">LOOM</span> et <span class=\"grad\">gagnez en encore plus</span>",
 			"en": "Delegate <span class=\"grad\">LOOM</span> and <span class=\"grad\">earn rewards</span>",
-			"jp": "<span class=\"grad\">#ハッシュ</span> と <span class=\"grad\">トレンドトピックでチャット</span>"
+			"jp": "<span class=\"grad\">#ハッシュ</span> と <span class=\"grad\">トレンドトピックでチャット</span>",
+	    		"pl": "Delegat <span class=\"grad\">LOOM</span> i <span class=\"grad\">zdobywaj nagrody</span>"
 		},
     "nfo": {
 			"fr": "Un jeu d'enfant, en 3 minutes, rentabilisez vos LOOM tant qu'il ne sont pas encore utiles.",
 			"en": "Nothing is easier, it only takes 3 minutes, make profit out of your LOOMs while you can't do anything with them so far.",
-			"jp": "簡単なことは何もありません、それは3分しかかかりません、あなたがこれまでのところ彼らと何もすることができない間あなたの織機から利益を得ます"
+			"jp": "簡単なことは何もありません、それは3分しかかかりません、あなたがこれまでのところ彼らと何もすることができない間あなたの織機から利益を得ます",
+	    		"pl": "To bardzo łatwe i zajmuje tylko 3 minuty, zarabiaj dzięki swoim LOOM-om"
 		},
 		"stake": {
 			"fr": "Stakez vos LOOM",
 			"en": "Stake with us!",
-			"jp": "私たちとステーク"
+			"jp": "私たちとステーク",
+			"pl": "Zarabiaj z nami"
 		},
 		"telegram": {
 			"fr": "Rejoindre sur Telegram",
 			"en": "Join us on Telegram",
-			"jp": "Telegram で私たちに参加"
+			"jp": "Telegram で私たちに参加",
+			"pl": "Dołącz do nas na Telegramie"
 		},
     "join": {
 			"fr": "Rejoignez nous sur Telegram pour poser des questions ou nous rencontrer: <span class=\"fluo\">#StakeHeyNetwork</span>",
 			"en": "Hey Loomers ! We, at <span class=\"fluo\">#Hey</span>, are almost ready to give you tons of information about <span class=\"fluo\">#Loom</span>, <span class=\"fluo\">#Staking</span> and <span class=\"fluo\">#Rewards</span>. Put your Looms at use and make some returns!",
-			"jp": "<span class=\"fluo\">#HeyNetwork</span> のディスカッションに参加しましょう"
+			"jp": "<span class=\"fluo\">#HeyNetwork</span> のディスカッションに参加しましょう",
+	    		"pl": "Hej Loomersi! My w <span class=\"fluo\">#Hey</span> jesteśmy prawie gotowi by dać was informacje <span class=\"fluo\">#Loom</span>, <span class=\"fluo\">#Zarobek</span> i <span class=\"fluo\">#Nagrody</span>. Wykorzystaj swoje LOOM-y by dzięki nim zarobić!"
 		},
 		"footer":{
 			"copyright":{
 				"fr": "Copyright",
 				"en": "Copyright",
-				"jp": "著作権"
+				"jp": "著作権",
+				"pl": "Prawa autorskie"
 			},
 			"rights":{
 				"fr": "Tous droits réservés.",
 				"en": "All rights Reserved.",
-				"jp": "全著作権所有"
+				"jp": "全著作権所有",
+				"pl": "Wszelkie prawa zastrzeżone."
 			},
 			"aboutUs":{
 				"fr": "A propos",
 				"en": "About us",
-				"jp": "私たちに関しては"
+				"jp": "私たちに関しては",
+				"pl": "O nas"
 			},
 			"manifesto":{
 				"fr": "Manifesto",
 				"en": "Manifesto",
-				"jp": "マニフェスト"
+				"jp": "マニフェスト",
+				"pl": "Manifest"
 			},
 			"support":{
 				"fr": "Support",
 				"en": "Support",
-				"jp": "補助"
+				"jp": "補助",
+				"pl": "Wsparcie"
 			},
 			"contactUs":{
 				"fr": "Contactez-nous",
 				"en": "Contact us",
-				"jp": "お問い合わせ"
+				"jp": "お問い合わせ",
+				"pl": "Kontakt"
 			},
 			"stakingGuide":{
 				"fr": "Guide de <em>Staking</em>",
 				"en": "Staking Guide",
-				"jp": "<em>Staking</em>ガイド"
+				"jp": "<em>Staking</em>ガイド",
+				"pl": "Przewodnik - jak zarobić"
 			},
 			"website":{
 				"fr": "Site web Hey",
 				"en": "Hey website",
-				"jp": "<em>Hey</em> ウェブサイト"
+				"jp": "<em>Hey</em> ウェブサイト",
+				"ok": "Strona Hey"
 			},
 			"dashboard":{
 				"fr": "Tableau de bord",
 				"en": "Dashboard",
-				"jp": "計器盤"
+				"jp": "計器盤",
+				"pl": "Panel"
 			}
 		}
 	}
